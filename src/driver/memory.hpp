3b8a02b8fb32b0c739d1120493b0d3eb51f9f72c
momo5502 <mauriceheumann@gmail.com>
2022-04-23 21:58:00 +0200

    Remove bad SAL annotation

--- src/driver/memory.hpp
+++ src/driver/memory.hpp
@@ -10,9 +10,6 @@ namespace memory
 	_IRQL_requires_max_(DISPATCH_LEVEL)
 	void* allocate_aligned_memory(size_t size);
 
-	_Must_inspect_result_
-	_IRQL_requires_max_(DISPATCH_LEVEL)
-
 	uint64_t get_physical_address(void* address);
 	void* get_virtual_address(uint64_t address);
 
